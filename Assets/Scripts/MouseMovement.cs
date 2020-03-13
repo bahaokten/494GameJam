d96f6524b64fca155001a28a6eb21cc4619cac26
floneyya <floneyya@umich.edu>
2020-03-13 19:07:34 -0400

    PlayerMovement

---
+++ Assets/Scripts/MouseMovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseMovement : MonoBehaviour
+{
+	Camera viewCamera;
+
+	void Start()
+	{
+		viewCamera = Camera.main;
+	}
+
+	void Update()
+	{
+		Vector3 mousePos = viewCamera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, viewCamera.transform.position.y));
+		transform.LookAt(mousePos + Vector3.up * transform.position.y);
+	}
+}
